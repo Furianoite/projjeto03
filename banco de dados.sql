49de50ab33e7ef3752eee70862c07a57052b4618
Caio Alex <128102270+Furianoite@users.noreply.github.com>
2023-06-07 21:43:55 -0300

    Create banco de dados.sql

---
+++ banco de dados.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE projetoa3;
+USE projetoa3;
+CREATE TABLE login(
+ nome VARCHAR(300),
+ login VARCHAR(100),
+ senha VARCHAR(100),
+ PRIMARY KEY (login)
+ );
+ projetoa3projetoa3  
+
+'INSERT INTO
+login(nome, login, senha)
+VALUES
+('administrador', 'admin','aluno123');
