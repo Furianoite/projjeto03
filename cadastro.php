5ab6c035080e1e6063fd6b4d2d77301c6d7ff40c
Caio Alex <128102270+Furianoite@users.noreply.github.com>
2023-06-15 21:29:20 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,10 +1,40 @@
 <?php
 include("conexao.php");
-$nome= isset($_POST['nome'])? $_POST['nome'] : '';
-$login= isset($_POST['login'])? $_POST['login'] : '';
-$senha= isset($_POST['senha'])? $_POST['senha'] : '';
-$insert="insert into login (nome,login,senha)
-values('$nome','$login','$senha')";
-$query= mysqli_query($conexao,$insert);
-header('location: index.php');
+
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+
+
+$verificaLogin = "SELECT * FROM login WHERE login = '$login'";
+$result = mysqli_query($conexao, $verificaLogin);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "Este login já está em uso. Por favor, escolha outro login.";
+} else {
+    $insert = "INSERT INTO login (nome, login, senha) VALUES ('$nome', '$login', '$senha')";
+    $query = mysqli_query($conexao, $insert);
+    header('location: index.php');
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Cadastro</title>
+    <style>
+        body {
+        background-image: url('https://i.pinimg.com/736x/e0/ef/2a/e0ef2ac8dbde9d21d7b68a189443cb78.jpg');
+        background-size: cover;
+        font-family: Arial, sans-serif;
+        color: #FFFFFF;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
